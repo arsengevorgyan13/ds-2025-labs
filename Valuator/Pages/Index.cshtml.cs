703f058e1065ecc696d2f79cfa2a56fc61e8f6da
arsengevorgyan13 <arsengevorgyanpgtu@gmail.com>
2025-03-06 09:24:29 +0300

    фиксы(ничего)

--- Valuator/Pages/Index.cshtml.cs
+++ Valuator/Pages/Index.cshtml.cs
@@ -57,16 +57,14 @@ public class IndexModel : PageModel
             if (!IsAlphabetic(c))
                 nonAlphabeticCount++;
         }
-        
-        double rank = nonAlphabeticCount / totalChars;
-        return rank;
+
+        return (double)nonAlphabeticCount / totalChars;
     }
 
     private double CountSimilarity(string text)
     {
         if (string.IsNullOrEmpty(text))
             return 0.0;
-        
         var server = _redisDb.Multiplexer.GetServer("localhost", 6379);
         var keys = server.Keys(database: _redisDb.Database, pattern: "TEXT-*");
 
